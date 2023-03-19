333086e6234f77b24782d5e4849da61a18cfd191
Oleg K <ok@vpngen.org>
2023-03-19 13:53:53 +0000

    Append ipsec statistics to stat api call

--- wg-mng.sh
+++ wg-mng.sh
@@ -302,11 +302,29 @@ case "${t}" in
                     echo "{\"code\": \"$?\"}"
                 else
                     echo -n "{\"code\": \"0\", \"traffic\": "
-                    ip netns exec "ns${wgi}" wg show "${wgi}" transfer 2>/dev/null | jq -R -s | tr -d '\n'
+                    join -j 1 -a 1 -a 2 -e 0 -o 0,1.2,1.3,2.2,2.3 \
+                        <(ip netns exec "ns${wgi}" wg show "${wgi}" transfer 2>/dev/null | tr "\t" " ") \
+                        <(join -j 1 -a 1 -e 0 -o 1.6,2.2,2.3 \
+                            <(cat /etc/accel-ppp.chap-secrets."${wgi}" | tr -d \" | sort -k1,1) \
+                            <(ip netns exec "ns${wgi}" accel-cmd -t 3 show sessions username,rx-bytes-raw,tx-bytes-raw | tail -n +3 | tr -d " \r" | tr "|" " " | sort -k 1,1 -u) \
+                            | sed "s/^#//") \
+                        | tr " " "\t" | jq -R -s | tr -d '\n'
                     echo -n ", \"last-seen\": "
-                    ip netns exec "ns${wgi}" wg show "${wgi}" latest-handshakes 2>/dev/null | jq -R -s | tr -d '\n'
+                    join -j 1 -a 1 -a 2 -e 0 -o 0,1.2,2.2 \
+                        <(ip netns exec "ns${wgi}" wg show "${wgi}" latest-handshakes 2>/dev/null | tr "\t" " ") \
+                        <(join -j 1 -a 1 -e 0 -o 1.6,2.2 \
+                            <(cat /etc/accel-ppp.chap-secrets."${wgi}" | tr -d \" | sort -k1,1) \
+                            <(ip netns exec "ns${wgi}" accel-cmd -t 3 show sessions username | tail -n +3 | tr -d " \r" | tr "|" " " | sed 's/$/ '`date +%s`'/'| sort -k 1,1 -u) \
+                            | sed "s/^#//") \
+                        | tr " " "\t" | jq -R -s | tr -d '\n'
                     echo -n ", \"endpoints\": "
-                    ip netns exec "ns${wgi}" wg show "${wgi}" endpoints 2>/dev/null | sed 's#\(\t[0-9]*\.[0-9]*\.[0-9]*\).*$#\1.0/24#g' | jq -R -s | tr -d '\n'
+                    join -j 1 -a 1 -a 2 -e "(none)" -o 0,1.2,2.2 \
+                        <(ip netns exec "ns${wgi}" wg show "${wgi}" endpoints 2>/dev/null | tr "\t" " ") \
+                        <(join -j 1 -a 1 -e "(none)" -o 1.6,2.2 \
+                            <(cat /etc/accel-ppp.chap-secrets."${wgi}" | tr -d \" | sort -k1,1) \
+                            <(ip netns exec "ns${wgi}" accel-cmd -t 3 show sessions username,calling-sid | tail -n +3 | tr -d " \r" | tr "|" " " | sort -k 1,1 -u) \
+                            | sed "s/^#//") \
+                        | tr " " "\t" | sed 's#\.[0-9]*:[0-9]*\t#.0/24\t#g' | sed 's#\.[0-9]*$#.0/24#g' | jq -R -s | tr -d '\n'
                     echo ", \"timestamp\": \"$(date +%s)\"}"
                 fi
         ;;
