83b98cba2b821360f9c8caa4c6a6d5b62ee1218e
Oleg K <ok@vpngen.org>
2024-07-25 10:35:50 +0000

    Disable ipsec and accel-pppd

--- wg-lea-rev.sh
+++ wg-lea-rev.sh
@@ -4,7 +4,7 @@ for i in `ip netns list | cut -d \  -f 1`; do
     (
         (
         ip netns exec "$i" wg show all dump | egrep "\.[0-9][0-9]?[0-9]?:[0-9]" | sed -E "s#(\.[0-9][0-9]?[0-9]?)\/[0-9].*#\1#g" ;
-        ip netns exec "$i" accel-cmd -4 -t 3 show sessions ifname,username,state,calling-sid,ip | tail -n +3 | tr -d '\|\r' ;
+#        ip netns exec "$i" accel-cmd -4 -t 3 show sessions ifname,username,state,calling-sid,ip | tail -n +3 | tr -d '\|\r' ;
         join -1 3 -2 2 -o 1.1,1.2,1.3,1.4,2.1 \
             <( join -1 5 -2 1 -o 1.1,1.2,1.3,2.2 \
                 <(grep -rH '^#' /opt/openvpn-${i:2}/ccd/ 2>/dev/null | sed 's#^.*/\([^/]*\):\##tun0 dummy \1 #' | sort -k5,5) \
